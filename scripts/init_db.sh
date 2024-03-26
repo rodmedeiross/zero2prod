c57e43d03174a702948f4e499aaf9ad38ac534eb
Rodrigo Medeiros <roddmedeiross@gmail.com>
2024-03-25 23:12:30 -0300

    fixes

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -33,8 +33,7 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
-if [[ -z "${SKIP_DOCKER}" ]]
-then
+if [[ -z "${SKIP_DOCKER}" ]]; then
   docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -50,7 +49,8 @@ export PGPASSWORD="${DB_PASSWORD}"
 
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
   >&2 echo "Postgres is still unavailable - sleeping"
-sleep 1 done
+  sleep 1 
+done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
