05a5807d651afc07fc4c3947619c2fde79208d3d
Rodrigo Medeiros <roddmedeiross@gmail.com>
2024-04-20 17:33:14 -0300

    fix pipeline, remove env from docker run

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -37,7 +37,7 @@ if [[ -z "${SKIP_DOCKER}" ]]; then
   docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
+    -e POSTGRES_DB=root \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
